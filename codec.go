53361bfb3a7952a091ceba32ce0cc830e01e76e5
buzzing <buzzing2017@gmail.com>
2024-12-26 15:55:50 +0800

    update comments

--- codec.go
+++ codec.go
@@ -3,12 +3,12 @@
 // 确保模块的消息、查询相应、状态存储等内容能够正确地序列化和反序列化
 //
 // 通常包括
-// + ProtoBuf 编码注册 RegisterCodec(cdc *codec.ProtoCodec)
-//   注册模块中定义的所有消息和结构体
-// + 接口注册 RegisterInterfaces(registry types.InterfaceRegistry)
-//   注册模块中的接口类型和它们的具体实现
-// + Codec 实例
-//   定义一个全局的 Codec 实例用于其他模块调用
+//   - ProtoBuf 编码注册 RegisterCodec(cdc *codec.ProtoCodec)
+//     注册模块中定义的所有消息和结构体
+//   - 接口注册 RegisterInterfaces(registry types.InterfaceRegistry)
+//     注册模块中的接口类型和它们的具体实现
+//   - Codec 实例
+//     定义一个全局的 Codec 实例用于其他模块调用
 package checkers
 
 import (
--- genesis.go
+++ genesis.go
@@ -3,10 +3,10 @@
 // 用于在区块链启动时加载模块的初始配置和数据
 //
 // 通常包括
-// + Genesis 数据结构的定义，这里位于 types.pb.go 中
-// + 初始化与导出逻辑
-//   初始化用于加载 Genesis 文件
-//   导出用于生成当前模块的状态快照
+//   - Genesis 数据结构的定义，这里位于 types.pb.go 中
+//   - 初始化与导出逻辑
+//     初始化用于加载 Genesis 文件
+//     导出用于生成当前模块的状态快照
 package checkers
 
 func NewGenesisState() GenesisState {
